01197b895148d7b264e09a15f8358df737b49272
Shahin Sheidaei <Shahin@Sheidaei.com>
2016-12-23 12:23:29 -0500

    added pug template, public folder for expressj, css for layering architecture, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # powerfulquestion
-This is a list of powerful questions, once every day!
+It all starts with a Question, a Powerful one! 
+
+Powerful Questions is an attempt to help curious minds to expand their horizons by helping them with open ended questions to ask. 
+
+You can visit its homepage at <a href="http://www.powerfulq.com">Powerful Question</a>
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var express=require('express');
 var app=express();
 var port = process.env.PORT || 3000
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(express.static('public'))
+
+
 app.get('/', function (req, res) {
-    getRandomLine( 'listofquestions.txt', function ( line ) { res.send( `<p style='font-size:4em'>${line}</p>` ); } );
+  //TODO make the file name passing not needed
+  getRandomLine( 'public/resources/listofquestions.txt', function ( line ) { res.render('index', { title: 'It all starts with a Question, a Powerful one!', message: `${line}` }) } );
 })
+
 /*--------------------Routing Over----------------------------*/
 
 function getRandomLine(filename, callback){
@@ -12,9 +19,10 @@ var fs = require('fs');
 fs.readFile(filename, function(err, data) {
     if(err) throw err;
     var array = data.toString().split("\n");
-    var length = Math.floor(Math.random() * array.length) + 1 ;
-        console.log(array[length]);
-        callback (array[length]);
+    //TODO make it a function
+    var randomline = Math.floor(Math.random() * array.length) + 1 ;
+        console.log(array[randomline]);
+        callback (array[randomline]);
 });
 }
 
